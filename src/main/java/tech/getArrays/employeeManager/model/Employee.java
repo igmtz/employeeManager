11f03830ecc065fce66584b9225f733929a4c9d2
Gonzalo Martínez <gmtzoliva29@gmail.com>
2022-07-10 16:22:43 -0500

    Initial commit: File creation.

---
+++ src/main/java/tech/getArrays/employeeManager/model/Employee.java
@@ -0,0 +1,10 @@
+package tech.getArrays.employeeManager.model;
+
+public class Employee {
+    private long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+}
